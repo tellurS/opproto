5fc356d111675f49031ced978a4c8355d624e688
Dmitry <dimachel@mail.ru>
2016-06-26 16:43:24 +0500

    Updates

---
+++ main.bundle.js
@@ -0,0 +1,539 @@
+webpackJsonp([3],{
+
+/***/ 0:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	/*
+	 * Providers provided by Angular
+	 */
+	var platform_browser_dynamic_1 = __webpack_require__(355);
+	/*
+	* Platform and Environment
+	* our providers/directives/pipes
+	*/
+	var browser_1 = __webpack_require__(546);
+	var environment_1 = __webpack_require__(547);
+	/*
+	* App Component
+	* our top level component that holds all of our components
+	*/
+	var app_1 = __webpack_require__(543);
+	/*
+	 * Bootstrap our Angular app with a top level component `App` and inject
+	 * our Services and Providers into Angular's dependency injection
+	 */
+	function main(initialHmrState) {
+	    return platform_browser_dynamic_1.bootstrap(app_1.App, browser_1.PLATFORM_PROVIDERS.concat(environment_1.ENV_PROVIDERS, app_1.APP_PROVIDERS))
+	        .then(environment_1.decorateComponentRef)
+	        .catch(function (err) { return console.error(err); });
+	}
+	exports.main = main;
+	/*
+	 * Vendors
+	 * For vendors for example jQuery, Lodash, angular2-jwt just import them anywhere in your app
+	 * You can also import them in vendors to ensure that they are bundled in one file
+	 * Also see custom-typings.d.ts as you also need to do `typings install x` where `x` is your module
+	 */
+	/*
+	 * Hot Module Reload
+	 * experimental version by @gdi2290
+	 */
+	if (false) {
+	    // activate hot module reload
+	    var ngHmr = require('angular2-hmr');
+	    ngHmr.hotModuleReplacement(main, module);
+	}
+	else {
+	    // bootstrap when document is ready
+	    document.addEventListener('DOMContentLoaded', function () { return main(); });
+	}
+	
+
+/***/ },
+
+/***/ 159:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	var core_1 = __webpack_require__(1);
+	var angular2_hmr_1 = __webpack_require__(386);
+	var AppState = (function () {
+	    function AppState() {
+	        // @HmrState() is used by HMR to track the state of any object during HMR (hot module replacement)
+	        this._state = {};
+	    }
+	    Object.defineProperty(AppState.prototype, "state", {
+	        // already return a clone of the current state
+	        get: function () {
+	            return this._state = this._clone(this._state);
+	        },
+	        // never allow mutation
+	        set: function (value) {
+	            throw new Error('do not mutate the `.state` directly');
+	        },
+	        enumerable: true,
+	        configurable: true
+	    });
+	    AppState.prototype.get = function (prop) {
+	        // use our state getter for the clone
+	        var state = this.state;
+	        return state.hasOwnProperty(prop) ? state[prop] : state;
+	    };
+	    AppState.prototype.set = function (prop, value) {
+	        // internally mutate our state
+	        return this._state[prop] = value;
+	    };
+	    AppState.prototype._clone = function (object) {
+	        // simple object clone
+	        return JSON.parse(JSON.stringify(object));
+	    };
+	    __decorate([
+	        angular2_hmr_1.HmrState(), 
+	        __metadata('design:type', Object)
+	    ], AppState.prototype, "_state", void 0);
+	    AppState = __decorate([
+	        core_1.Injectable(), 
+	        __metadata('design:paramtypes', [])
+	    ], AppState);
+	    return AppState;
+	}());
+	exports.AppState = AppState;
+	
+
+/***/ },
+
+/***/ 371:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	/*
+	 * These are globally available directives in any template
+	 */
+	// Angular 2
+	var core_1 = __webpack_require__(1);
+	// Angular 2 Router
+	var router_1 = __webpack_require__(243);
+	// Angular 2 forms
+	var forms_1 = __webpack_require__(219);
+	// application_directives: directives that are global through out the application
+	exports.APPLICATION_DIRECTIVES = router_1.ROUTER_DIRECTIVES.concat(forms_1.REACTIVE_FORM_DIRECTIVES);
+	exports.DIRECTIVES = [
+	    { provide: core_1.PLATFORM_DIRECTIVES, multi: true, useValue: exports.APPLICATION_DIRECTIVES }
+	];
+	
+
+/***/ },
+
+/***/ 372:
+/***/ function(module, exports, __webpack_require__) {
+
+	/*
+	 * These are globally available pipes in any template
+	 */
+	"use strict";
+	var core_1 = __webpack_require__(1);
+	// application_pipes: pipes that are global through out the application
+	exports.APPLICATION_PIPES = [];
+	exports.PIPES = [
+	    { provide: core_1.PLATFORM_PIPES, multi: true, useValue: exports.APPLICATION_PIPES }
+	];
+	
+
+/***/ },
+
+/***/ 373:
+/***/ function(module, exports, __webpack_require__) {
+
+	/*
+	 * These are globally available services in any component or any other service
+	 */
+	"use strict";
+	// Angular 2
+	var common_1 = __webpack_require__(35);
+	// Angular 2 Http
+	var http_1 = __webpack_require__(231);
+	// Angular 2 Router
+	var router_1 = __webpack_require__(243);
+	// Angular 2 forms
+	var forms_1 = __webpack_require__(219);
+	// AngularClass
+	var webpack_toolkit_1 = __webpack_require__(370);
+	var request_idle_callback_1 = __webpack_require__(369);
+	var app_routes_1 = __webpack_require__(536);
+	/*
+	* Application Providers/Directives/Pipes
+	* providers/directives/pipes that only live in our browser environment
+	*/
+	exports.APPLICATION_PROVIDERS = [
+	    // new Angular 2 forms
+	    forms_1.disableDeprecatedForms(),
+	    forms_1.provideForms(),
+	    router_1.provideRouter(app_routes_1.routes),
+	    webpack_toolkit_1.provideWebpack(app_routes_1.asyncRoutes),
+	    request_idle_callback_1.providePrefetchIdleCallbacks(app_routes_1.prefetchRouteCallbacks)
+	].concat(http_1.HTTP_PROVIDERS, [
+	    { provide: common_1.LocationStrategy, useClass: common_1.HashLocationStrategy }
+	]);
+	exports.PROVIDERS = exports.APPLICATION_PROVIDERS.slice();
+	
+
+/***/ },
+
+/***/ 535:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	var core_1 = __webpack_require__(1);
+	var app_service_1 = __webpack_require__(159);
+	/*
+	 * App Component
+	 * Top Level Component
+	 */
+	var App = (function () {
+	    function App(appState) {
+	        this.appState = appState;
+	    }
+	    App.prototype.ngOnInit = function () {
+	        console.log('Initial App State', this.appState.state);
+	    };
+	    App = __decorate([
+	        core_1.Component({
+	            selector: 'app',
+	            encapsulation: core_1.ViewEncapsulation.None,
+	            styles: [
+	                __webpack_require__(755)
+	            ],
+	            template: "\n    <nav>\n      <span>\n        <a [routerLink]=\" ['./'] \">\n          Index\n        </a>\n      </span>\n      |\n      <span>\n        <a [routerLink]=\" ['./home'] \">\n          Home\n        </a>\n      </span>\n      |\n      <span>\n        <a [routerLink]=\" ['./about'] \">\n          About\n        </a>\n      </span>\n    </nav>\n\n    <main>\n      <router-outlet></router-outlet>\n    </main>\n\n    <pre class=\"app-state\">this.appState.state = {{ appState.state | json }}</pre>\n\n    <footer>\n      <span>Op proto</span>\n \n    </footer>\n  "
+	        }), 
+	        __metadata('design:paramtypes', [app_service_1.AppState])
+	    ], App);
+	    return App;
+	}());
+	exports.App = App;
+	
+
+/***/ },
+
+/***/ 536:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	var home_1 = __webpack_require__(538);
+	var no_content_1 = __webpack_require__(544);
+	exports.routes = [
+	    { path: '', component: home_1.Home },
+	    { path: 'home', component: home_1.Home },
+	    // make sure you match the component type string to the require in asyncRoutes
+	    { path: 'about', component: 'About' },
+	    { path: '**', component: no_content_1.NoContent },
+	];
+	// Async load a component using Webpack's require with es6-promise-loader and webpack `require`
+	// asyncRoutes is needed for our @angularclass/webpack-toolkit that will allow us to resolve
+	// the component correctly
+	exports.asyncRoutes = {
+	    'About': __webpack_require__(548)
+	};
+	// Optimizations for initial loads
+	// An array of callbacks to be invoked after bootstrap to prefetch async routes
+	exports.prefetchRouteCallbacks = [
+	    exports.asyncRoutes['About'] // es6-promise-loader returns a function
+	];
+	// Es6PromiseLoader and AsyncRoutes interfaces are defined in custom-typings
+	
+
+/***/ },
+
+/***/ 537:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	var core_1 = __webpack_require__(1);
+	var app_service_1 = __webpack_require__(159);
+	var title_1 = __webpack_require__(539);
+	var x_large_1 = __webpack_require__(541);
+	var Home = (function () {
+	    // TypeScript public modifiers
+	    function Home(appState, title) {
+	        this.appState = appState;
+	        this.title = title;
+	        // Set our default values
+	        this.localState = { value: '' };
+	    }
+	    Home.prototype.ngOnInit = function () {
+	        console.log('hello `Home` component');
+	        // this.title.getData().subscribe(data => this.data = data);
+	    };
+	    Home.prototype.submitState = function (value) {
+	        console.log('submitState', value);
+	        this.appState.set('value', value);
+	        this.localState.value = '';
+	    };
+	    Home = __decorate([
+	        core_1.Component({
+	            // The selector is what angular internally uses
+	            // for `document.querySelectorAll(selector)` in our index.html
+	            // where, in this case, selector is the string 'home'
+	            selector: 'home',
+	            // We need to tell Angular's Dependency Injection which providers are in our app.
+	            providers: [
+	                title_1.Title
+	            ],
+	            // We need to tell Angular's compiler which directives are in our template.
+	            // Doing so will allow Angular to attach our behavior to an element
+	            directives: [
+	                x_large_1.XLarge
+	            ],
+	            // We need to tell Angular's compiler which custom pipes are in our template.
+	            pipes: [],
+	            // Our list of styles in our component. We may add more to compose many styles together
+	            styles: [__webpack_require__(756)],
+	            // Every Angular template is first compiled by the browser before Angular runs it's compiler
+	            template: __webpack_require__(549)
+	        }), 
+	        __metadata('design:paramtypes', [app_service_1.AppState, title_1.Title])
+	    ], Home);
+	    return Home;
+	}());
+	exports.Home = Home;
+	
+
+/***/ },
+
+/***/ 538:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	function __export(m) {
+	    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+	}
+	__export(__webpack_require__(537));
+	
+
+/***/ },
+
+/***/ 539:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	function __export(m) {
+	    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+	}
+	__export(__webpack_require__(540));
+	
+
+/***/ },
+
+/***/ 540:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	var core_1 = __webpack_require__(1);
+	var http_1 = __webpack_require__(231);
+	var Title = (function () {
+	    function Title(http) {
+	        this.http = http;
+	        this.value = 'Angular 2';
+	    }
+	    Title.prototype.getData = function () {
+	        console.log('Title#getData(): Get Data');
+	        // return this.http.get('/assets/data.json')
+	        // .map(res => res.json());
+	        return {
+	            value: 'AngularClass'
+	        };
+	    };
+	    Title = __decorate([
+	        core_1.Injectable(), 
+	        __metadata('design:paramtypes', [http_1.Http])
+	    ], Title);
+	    return Title;
+	}());
+	exports.Title = Title;
+	
+
+/***/ },
+
+/***/ 541:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	function __export(m) {
+	    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+	}
+	__export(__webpack_require__(542));
+	
+
+/***/ },
+
+/***/ 542:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	var core_1 = __webpack_require__(1);
+	/*
+	 * Directive
+	 * XLarge is a simple directive to show how one is made
+	 */
+	var XLarge = (function () {
+	    function XLarge(element, renderer) {
+	        // simple DOM manipulation to set font size to x-large
+	        // `nativeElement` is the direct reference to the DOM element
+	        // element.nativeElement.style.fontSize = 'x-large';
+	        // for server/webworker support use the renderer
+	        renderer.setElementStyle(element.nativeElement, 'fontSize', 'x-large');
+	    }
+	    XLarge = __decorate([
+	        core_1.Directive({
+	            selector: '[x-large]' // using [ ] means selecting attributes
+	        }), 
+	        __metadata('design:paramtypes', [core_1.ElementRef, core_1.Renderer])
+	    ], XLarge);
+	    return XLarge;
+	}());
+	exports.XLarge = XLarge;
+	
+
+/***/ },
+
+/***/ 543:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	function __export(m) {
+	    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+	}
+	// App
+	__export(__webpack_require__(535));
+	__export(__webpack_require__(159));
+	var app_service_2 = __webpack_require__(159);
+	// Application wide providers
+	exports.APP_PROVIDERS = [
+	    app_service_2.AppState
+	];
+	
+
+/***/ },
+
+/***/ 544:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	function __export(m) {
+	    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+	}
+	__export(__webpack_require__(545));
+	
+
+/***/ },
+
+/***/ 545:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	var core_1 = __webpack_require__(1);
+	var NoContent = (function () {
+	    function NoContent() {
+	    }
+	    NoContent = __decorate([
+	        core_1.Component({
+	            selector: 'no-content',
+	            template: "\n    <div>\n      <h1>404: page missing</h1>\n    </div>\n  "
+	        }), 
+	        __metadata('design:paramtypes', [])
+	    ], NoContent);
+	    return NoContent;
+	}());
+	exports.NoContent = NoContent;
+	
+
+/***/ },
+
+/***/ 546:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	function __export(m) {
+	    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+	}
+	__export(__webpack_require__(371));
+	__export(__webpack_require__(372));
+	__export(__webpack_require__(373));
+	var browser_directives_2 = __webpack_require__(371);
+	var browser_pipes_2 = __webpack_require__(372);
+	var browser_providers_2 = __webpack_require__(373);
+	exports.PLATFORM_PROVIDERS = browser_providers_2.PROVIDERS.concat(browser_directives_2.DIRECTIVES, browser_pipes_2.PIPES);
+	
+
+/***/ },
+
+/***/ 547:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	// Angular 2
+	// rc2 workaround
+	var platform_browser_1 = __webpack_require__(112);
+	var core_1 = __webpack_require__(1);
+	// Environment Providers
+	var PROVIDERS = [];
+	// Angular debug tools in the dev console
+	// https://github.com/angular/angular/blob/86405345b781a9dc2438c0fbe3e9409245647019/TOOLS_JS.md
+	var _decorateComponentRef = function identity(value) { return value; };
+	if (false) {
+	    // Production
+	    platform_browser_1.disableDebugTools();
+	    core_1.enableProdMode();
+	    PROVIDERS = PROVIDERS.slice();
+	}
+	else {
+	    _decorateComponentRef = function (cmpRef) { return platform_browser_1.enableDebugTools(cmpRef); };
+	    // Development
+	    PROVIDERS = PROVIDERS.slice();
+	}
+	exports.decorateComponentRef = _decorateComponentRef;
+	exports.ENV_PROVIDERS = PROVIDERS.slice();
+	
+
+/***/ },
+
+/***/ 548:
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = function (namespace) {
+	  return new Promise(function (resolve) {
+	    __webpack_require__.e/* nsure */(1, function (require) {
+	      if (namespace) {
+	        resolve(__webpack_require__(422)[namespace]);
+	      } else {
+	        resolve(__webpack_require__(422));
+	      }
+	    });
+	  });
+	}
+
+/***/ },
+
+/***/ 549:
+/***/ function(module, exports) {
+
+	module.exports = "<div class=\"card-container\">\n  <h1 x-large class=\"sample-content\">Your Content Here</h1>\n\n\n  <hr>\n\n  <div>\n    For hot module reloading run\n    <pre>npm run start:hmr</pre>\n  </div>\n\n  <hr>\n\n  <div>\n    <h4>Local State</h4>\n\n    <form (ngSubmit)=\"submitState(localState.value)\" autocomplete=\"off\">\n\n      <input\n        [value]=\"localState.value\"\n        (input)=\"localState.value = $event.target.value\"\n        placeholder=\"Submit Local State to App State\"\n        autofocus>\n\n      <button md-raised-button color=\"primary\">Submit Value</button>\n    </form>\n    <!--\n        <input type=\"text\" [value]=\"localState.value\" (input)=\"localState.value = $event.target.value\" autofocus>\n        Rather than wiring up two-way data-binding ourselves with [value] and (input)\n        we can use Angular's [(ngModel)] syntax\n        <input type=\"text\" name=\"textInput\" [(ngModel)]=\"localState.value\" autofocus>\n      -->\n\n    <pre>this.localState = {{ localState | json }}</pre>\n\n  </div>\n\n</div>\n"
+
+/***/ },
+
+/***/ 755:
+/***/ function(module, exports) {
+
+	module.exports = "html, body{\n  height: 100%;\n  font-family: Arial, Helvetica, sans-serif\n}\n\nspan.active {\n  background-color: gray;\n}\n"
+
+/***/ },
+
+/***/ 756:
+/***/ function(module, exports) {
+
+	module.exports = "/*styles for home content only*/"
+
+/***/ }
+
+});
+//# sourceMappingURL=main.map
