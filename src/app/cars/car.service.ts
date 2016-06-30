1aa45f79daddcd14288dbab9e238506350ab50a4
pdn <pdn@localhost>
2016-06-30 09:43:38 +0300

    +id=>caption

--- src/app/cars/car.service.ts
+++ src/app/cars/car.service.ts
@@ -2,33 +2,47 @@ import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/delay';
 
-
-import { Car } from './car';
+import { Car,CarColor } from './car';
  import 'rxjs/Rx';
    
 @Injectable()
 export class CarService {
-    data={"data":[        
-            {"brand": "VW", "year": 2012, "color": "White", "vin": "dsad231ff"},
-            {"brand": "Audi", "year": 2011, "color": "Black", "vin": "gwregre345"},
-            {"brand": "Renault", "year": 2005, "color": "Gray", "vin": "h354htr"},
-            {"brand": "BMW", "year": 2003, "color": "Blue", "vin": "j6w54qgh"},
-            {"brand": "Mercedes", "year": 1995, "color": "White", "vin": "hrtwy34"},
-            {"brand": "Volvo", "year": 2005, "color": "Black", "vin": "jejtyj"},
-            {"brand": "Honda", "year": 2012, "color": "Yellow", "vin": "g43gr"},
-            {"brand": "Jaguar", "year": 2013, "color": "White", "vin": "greg34"},
-            {"brand": "Ford", "year": 2000, "color": "Black", "vin": "h54hw5"},
-            {"brand": "Fiat", "year": 2013, "color": "Red", "vin": "245t2s"}
+    private carsData={"data":[        
+            {"brand": "VW", "year": 2012, "colorId": 1, "vin": "dsad231ff"},
+            {"brand": "Audi", "year": 2011, "colorId": 2, "vin": "gwregre345"},
+            {"brand": "Renault", "year": 2005, "colorId": 3, "vin": "h354htr"},
+            {"brand": "BMW", "year": 2003, "colorId": 2, "vin": "j6w54qgh"},
+            {"brand": "Mercedes", "year": 1995, "colorId": 2, "vin": "hrtwy34"},
+            {"brand": "Volvo", "year": 2005, "colorId": 1, "vin": "jejtyj"},
+            {"brand": "Honda", "year": 2012, "colorId": 1, "vin": "g43gr"},
+            {"brand": "Jaguar", "year": 2013, "colorId": 3, "vin": "greg34"},
+            {"brand": "Ford", "year": 2000, "colorId": 3, "vin": "h54hw5"},
+            {"brand": "Fiat", "year": 2013, "colorId": 1, "vin": "245t2s"}
            ]};
+    private carsColor={"data":[        
+            {"colorId": 1, "color": "Red"},
+            {"colorId": 2, "color": "Blue"},
+            {"colorId": 3, "color": "Black"},
+           ]};           
+           
     constructor(/*private http: Http*/) {}
-
+    
+    
     getCarsSmall():Observable <Car[]> {
         /*this.http.get('/showcase/resources/data/cars-small.json')
                  .retry(5)
                  .map(res => res.json())
                 */
-        return Observable.of(this.data)
-            .map(res => <Car[]> res.data);
-        }
+                
+        this.getCarsColor().subscribe(r=> Car.colors=r);
+        
+        return Observable.of(this.carsData)
+               .map(res => res.data.map(el => new Car(el))).delay(400);; 
+        }    
+    getCarsColor():Observable <CarColor[]> {
+        return Observable.of(this.carsColor)
+               .map(res => res.data.map(e => new CarColor(e))).delay(1000); 
+    }        
 }
--- src/app/cars/car.ts
+++ src/app/cars/car.ts
@@ -1,6 +1,33 @@
-export interface Car {
-    vin;
-    year;
-    brand;
-    color;
+export class CarData{
+    vin:string;
+    year:number;
+    brand:string;
+    colorId:number
+}
+
+export class Car extends CarData{    
+    private static id2color:any={};        
+    public static set colors(colors:CarColor[]){
+        colors.map(e => Car.id2color[e.colorId] = e.color);
+    }
+    
+    public get color():String{
+        return Car.id2color[this.colorId]||"not setting";
+    }
+    constructor(data:any) { 
+        super();
+        Object.assign(this, data);
+    }
+}
+
+export class CarColorData{
+    colorId:number;
+    color:string;
+}
+
+export class CarColor extends CarColorData{    
+    constructor(data:any) { 
+        super();
+        Object.assign(this, data);
+    }
 }
